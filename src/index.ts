6a4485cad938ec3c2a77ef821ef2be5ae065fdd0
priyanshpatel18 <priyansh.180804@gmail.com>
2024-09-23 13:15:10 +0530

    response changes

--- src/index.ts
+++ src/index.ts
@@ -48,31 +48,39 @@ app.post('/run', async (req: Request, res: Response) => {
   }
 
   const container = selectAvailableContainer();
+  console.log(container);
+
   if (!container) {
     // Push the body to the queue
     return res
-      .status(503)
+      .status(400)
       .json({ result: 'All containers are busy.', success: false });
   }
 
   try {
     container.busy = true;
-    // const response = await fetch(`http://${container.ip}:${container.port}/execute`, {
-    const response = await fetch(`http://localhost:5555/execute`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      `http://${container.ip}:${container.port}/execute`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code, language }),
       },
-      body: JSON.stringify({ code, language }),
-    });
-    const { result } = await response.json();
-
-    if (result) {
-      const cleanedOutput = stripAnsiCodes(result);
-      res.status(200).json({ result: cleanedOutput, success: true });
-    } else {
-      res.status(500).json({ result: 'Failed to run code.', success: false });
+    );
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      return res.status(response.status).json({
+        result: errorResponse.message || 'Failed to run code.',
+        success: false,
+      });
     }
+
+    const { result } = await response.json();
+    
+    const cleanedOutput = stripAnsiCodes(result);
+    res.status(200).json({ result: cleanedOutput, success: true });
   } catch (error) {
     console.log(error);
     res.status(500).json({ result: 'Internal Server Error', success: false });
